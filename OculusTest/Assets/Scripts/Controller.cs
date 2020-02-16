99ef752b311c7a1f237165cfb38b2158bea79ba0
AlexOToole <tooler1999@gmail.com>
2020-02-16 12:57:53 +0000

    added all progress from my personal pc, that has not been pushed yet

--- OculusTest/Assets/Scripts/Controller.cs
+++ OculusTest/Assets/Scripts/Controller.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 //3 box script
 // each with access to game controller script
@@ -14,25 +14,74 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    public GameObject[] Boxs;
     public GameObject[] shapes;
-    public static string[] colourAt;
-    public static string[] inputOrder;
-    public static string[] correctOrder = { "Red", "Blue", "Blue", "Green", "Green", "Red" };
+    //public string[] colourAt;
+    public List<string> inputOrder;
+    public string[] correctOrder = { "Red", "Blue", "Blue", "Green", "Green", "Red" };
+    bool correct = false;
+    public float targetTime = 10.0f;
+    public Text titleText;
+    public Text orderText;
     // Start is called before the first frame update
     void Start()
     {
-        //Colour od the shapes in order from left to right.
-        colourAt[0] = "Red";
-        colourAt[1] = "Green";
-        colourAt[2] = "Green";
-        colourAt[3] = "Blue";
-        colourAt[4] = "Red";
-        colourAt[5] = "Blue";
+        //Colour of the shapes in order from left to right.
+        //colourAt[0] = "Red";
+        //colourAt[1] = "Green";
+        //colourAt[2] = "Green";
+        //colourAt[3] = "Blue";
+        //colourAt[4] = "Red";
+        //colourAt[5] = "Blue";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Timer Code - stops once its less than 0;
+        if (targetTime > 0)
+        {
+            targetTime -= Time.deltaTime;
+        }
+        if (targetTime <= 0)
+        {
+            titleText.gameObject.SetActive(false);
+            orderText.text = "The correct order is \n";
+            for (int i =0; i < correctOrder.Length; i++)
+            {
+                orderText.text += correctOrder[i] + "\n";
+            }
+            //When the input order list contains 6 entries, check the answer.
+            if (inputOrder.Count == 6)
+            {
+                checkAnswer();
+            }
+            if (correct)
+            {
+                Debug.Log("Win");
+            }
+            else
+            {
+                Debug.Log("Lose");
+            }
+        }
+    }
+
+    void checkAnswer()
+    {
+        if (inputOrder.Count == 6)
+        {
+            for (int i = 0; i < correctOrder.Length; i++)
+            {
+                if (inputOrder[i] == correctOrder[i])
+                {
+                    correct = true;
+                }
+                else
+                {
+                    correct = false;
+                }
+            }
+        }
     }
 }
---
+++ OculusTest/Assets/Scripts/Cube.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    GameObject gameController;
+    public string color;
+    public Material Grey;
+    Material startMaterial;
+    public bool inBox = false;
+    public int index = -1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = GameObject.FindWithTag("GameController");
+        startMaterial = gameObject.GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameController.GetComponent<Controller>().targetTime > 0.0f)
+        {
+            gameObject.GetComponent<OVRGrabbable>().enabled = false;
+        }
+        else
+        {
+            gameObject.GetComponent<OVRGrabbable>().enabled = true;
+            RemoveMaterial();
+        }
+    }
+
+    //Function that changes the cube material to a grey one.
+    public void RemoveMaterial()
+    {
+        gameObject.GetComponent<MeshRenderer>().material = Grey;
+    }
+
+    //Function that changes the ube material to its starting material.
+    public void ResetMaterial()
+    {
+        gameObject.GetComponent<MeshRenderer>().material = startMaterial;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "CustomHandLeft" || collision.gameObject.name == "CustomHandRight")
+        {
+            //check to see if list isnt empty
+            if (gameController.GetComponent<Controller>().inputOrder.Count > 0)
+            {
+                if (index > -1)
+                {
+                    gameController.GetComponent<Controller>().inputOrder.RemoveAt(index);
+                }
+            }
+        }
+    }
+}
