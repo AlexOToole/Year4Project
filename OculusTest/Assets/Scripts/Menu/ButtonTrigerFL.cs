97fd0807473edff7a50e9c8d3ec538cbc14a59e7
AlexOToole <C00216095@itcarlow.ie>
2020-04-23 18:54:45 +0100

    Added build file

--- OculusTest/Assets/Scripts/Menu/ButtonTrigerFL.cs
+++ OculusTest/Assets/Scripts/Menu/ButtonTrigerFL.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class ButtonTrigerFL : MonoBehaviour
 {
-{
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Button"))
